ea8f5c99796bff6dfff2c0f61f20ad8893fde509
Sophie Labyt <labytsophie@gmail.com>
2024-02-23 08:58:22 +0100

    correction html + script

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,4 +1,5 @@
-// Récupération des travaux depuis API 
+const imagesContainer = document.querySelector('.gallery');
+ 
 async function main(){
     const reponse = await fetch('http://localhost:5678/api/works');
     works = await reponse.json();
