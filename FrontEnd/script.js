7166dc9a6542fafe43fd3b9a898753437d1abc3a
Sophie Labyt <labytsophie@gmail.com>
2024-02-23 08:49:10 +0100

    correction: recuperation travaux API

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,21 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('http://localhost:5678/api-docs/') 
-        .then(response => response.json())
-        .then(data => {
-            const galerie = document.querySelector('.gallery');
-            data.forEach(projet => {
-                const figure = document.createElement('figure');
-                const img = document.createElement('img');
-                const figcaption = document.createElement('figcaption');
-                
-                img.src = projet.image; 
-                img.alt = projet.titre; 
-                figcaption.textContent = projet.titre;
-
-                figure.appendChild(img);
-                figure.appendChild(figcaption);
-                galerie.appendChild(figure);
-            });
-        })
-        .catch(error => console.error('Erreur lors de la récupération des données:', error));
-});
+// Récupération des travaux depuis API 
+async function main(){
+    const reponse = await fetch('http://localhost:5678/api/works');
+    works = await reponse.json();
+    generateWorks(works);
