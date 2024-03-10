f715b1c28b78cebebc5b96ea4d2cbe7b3430b438
Sophie Labyt <labytsophie@gmail.com>
2024-03-10 17:38:54 +0100

    script: Filter Fix: Activating the "All" Button

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -7,17 +7,19 @@ const imagesContainer = document.querySelector('.gallery');
 const filtersContainer = document.querySelector('.filters');
 
 // Afficher les travaux
-function fetchAndDisplayWorks(category = '') {
+function fetchAndDisplayWorks(category) {
+    //ne pas oublier de mettre le paramètre category dans la fonction
     let url = 'http://localhost:5678/api/works';
-    if (category && category !== 'Tous') {
-        // Appliquer le filtre de catégorie
-        url += `?category=${encodeURIComponent(category)}`;
-    }
-
     fetch(url)
         .then(response => response.json())
         .then(works => {
-            displayWorks(works);
+            if(category === undefined || category === "Tous") {
+                displayWorks(works)
+            } else {
+
+                let newWorksArray = works.filter((work) => work.category.name === category)
+                displayWorks(newWorksArray)
+            }
         })
         .catch(error => console.error('Erreur lors de la récupération des travaux:', error));
 }
@@ -45,6 +47,14 @@ function initializeFilters() {
         .then(response => response.json())
         .then(categories => {
             filtersContainer.innerHTML = '<button class="filter-btn" data-filter="Tous">Tous</button>'; // Bouton pour tous les travaux
+            
+            
+            // Ajout de l'écouteur d'événement au bouton "Tous"
+            const allButton = filtersContainer.querySelector('.filter-btn[data-filter="Tous"]');
+            allButton.addEventListener('click', function () {
+                fetchAndDisplayWorks('Tous'); // Ou un argument approprié pour afficher tous les travaux
+            });
+            
             categories.forEach(category => {
                 const button = document.createElement('button');
                 button.textContent = category.name; // Utilisation de 'name' si l'API retourne un objet
