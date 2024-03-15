c21d4ccbe6011b2935dd9c7c2338bc445df2567d
Sophie Labyt <labytsophie@gmail.com>
2024-03-15 13:53:34 +0100

    Button filter-btn and correction login.html

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -4,14 +4,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const loginLink = document.querySelector('.login__link');
     const token = localStorage.getItem('token');
-    
+
     // Vérification si l'utilisateur est déjà connecté
     if (token) {
         loginLink.textContent = 'Logout';
     } else {
         loginLink.textContent = 'Login';
     }
-    
+
     loginLink.addEventListener('click', (e) => {
         if (loginLink.textContent === 'Logout') {
             e.preventDefault();
@@ -41,26 +41,26 @@ document.addEventListener('DOMContentLoaded', () => {
             },
             body: JSON.stringify(user)
         })
-        .then(response => {
-            /*if (!response.ok) {
-                throw new Error('Erreur d’identification. Veuillez vérifier votre email ou mot de passe.');
-            }*/
-            return response.json();
-        })
-        .then(data => {
-            localStorage.setItem('token', data.token);
-            // Mettre à jour le texte du lien 
-            loginLink.textContent = 'Logout';
-            // Redirection vers la page d'accueil après connexion réussie
-            window.location.href = 'index.html';
-        })
-        .catch(error => {
-            console.error("Echec de l'authentification :", error);
-            messageError.textContent = error.message;
-            messageError.style.display = 'block';
-            setTimeout(() => {
-                messageError.style.display = 'none';
-            }, 5000);
-        });
+            .then(response => {
+                /*if (!response.ok) {
+                    throw new Error('Erreur d’identification. Veuillez vérifier votre email ou mot de passe.');
+                }*/
+                return response.json();
+            })
+            .then(data => {
+                localStorage.setItem('token', data.token);
+                // Mettre à jour le texte du lien 
+                loginLink.textContent = 'Logout';
+                // Redirection vers la page d'accueil après connexion réussie
+                window.location.href = 'index.html';
+            })
+            .catch(error => {
+                console.error("Echec de l'authentification :", error);
+                messageError.textContent = error.message;
+                messageError.style.display = 'block';
+                setTimeout(() => {
+                    messageError.style.display = 'none';
+                }, 5000);
+            });
     });
 });
--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -8,12 +8,11 @@ const filtersContainer = document.querySelector('.filters');
 
 // Afficher les travaux
 function fetchAndDisplayWorks(category) {
-    //ne pas oublier de mettre le paramètre category dans la fonction
     let url = 'http://localhost:5678/api/works';
     fetch(url)
         .then(response => response.json())
         .then(works => {
-            if(category === undefined || category === "Tous") {
+            if (category === undefined || category === "Tous") {
                 displayWorks(works)
             } else {
 
@@ -46,18 +45,18 @@ function initializeFilters() {
     fetch('http://localhost:5678/api/categories')
         .then(response => response.json())
         .then(categories => {
-            filtersContainer.innerHTML = '<button class="filter-btn" data-filter="Tous">Tous</button>'; // Bouton pour tous les travaux
-            
-            
+            filtersContainer.innerHTML = '<button class="filter-btn" data-filter="Tous">Tous</button>';// Bouton pour tous les travaux
+
+
             // Ajout de l'écouteur d'événement au bouton "Tous"
             const allButton = filtersContainer.querySelector('.filter-btn[data-filter="Tous"]');
             allButton.addEventListener('click', function () {
-                fetchAndDisplayWorks('Tous'); // Ou un argument approprié pour afficher tous les travaux
+                fetchAndDisplayWorks('Tous');
             });
-            
+
             categories.forEach(category => {
                 const button = document.createElement('button');
-                button.textContent = category.name; // Utilisation de 'name' si l'API retourne un objet
+                button.textContent = category.name;
                 button.className = 'filter-btn';
                 button.setAttribute('data-filter', category.name);
                 button.addEventListener('click', function () {
